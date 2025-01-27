132807218edf2f74c0d7c6169fe50b73cf48d497
sunohkim <sunoh1222@gmail.com>
2025-01-27 20:41:12 +0900

    Setting: 백엔드 초기 환경 설정 #3 - 필수 라이브러리를 적용해 백엔드 프로젝트를 생성 - Spring Web, Spring Data JPA, Lombok, Validation, MySQL Driver 라이브러리를 추가

---
+++ back-end/reacton/src/main/java/com/softeer/reacton/ReactonApplication.java
@@ -0,0 +1,13 @@
+package com.softeer.reacton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactonApplication.class, args);
+	}
+
+}
