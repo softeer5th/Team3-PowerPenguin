d3c7f056ae3794a15ea8b97208024a5f5c9253dc
uri010 <dbfl0461@gmail.com>
2025-01-30 15:37:07 +0900

    Feat: Request 엔티티, 컨트롤러, 서비스, 레포지토리 추가 #8

---
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/request/Request.java
@@ -0,0 +1,21 @@
+package com.softeer.reacton.domain.request;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "request")
+@Entity
+public class Request {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String type;
+
+    @Column(nullable = false, columnDefinition = "INT DEFAULT 0")
+    private int count;
+}
