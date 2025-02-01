042eb7d63cbcb97a7154a14150a60b669945ce5b
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-02-01 19:36:47 +0900

    Feat: Text Button UI 추상화 완성 #5
    - TextButton 추상화 방식 구체화
    - "수업 끝내기" 버튼 분리
    - 스타일 및 컴포넌트 정리

--- front-end/src/components/button/text/TextButton.tsx
+++ front-end/src/components/button/text/TextButton.tsx
@@ -1,21 +1,64 @@
 import S from './TextButton.module.css';
 
 type TextButtonProps = {
-  style: CSSModuleClasses;
+  color: 'blue' | 'red' | 'green' | 'black' | 'white' | 'inherit';
+  size: 'web1' | 'web2' | 'web3' | 'web4' | 'mobile1' | 'mobile2';
   text: string;
   isActive?: boolean;
   onClick: () => void;
 };
 
 const TextButton = ({
-  style,
+  color,
+  size,
   text,
   onClick,
   isActive = true,
-}: TextButtonProps) => {
+}: TextButtonProps): JSX.Element => {
+  let colorClass, sizeClass;
+  switch (color) {
+    case 'blue':
+      colorClass = S.blueButton;
+      break;
+    case 'red':
+      colorClass = S.redButton;
+      break;
+    case 'green':
+      colorClass = S.greenButton;
+      break;
+    case 'black':
+      colorClass = S.blackButton;
+      break;
+    case 'white':
+      colorClass = S.whiteButton;
+      break;
+    case 'inherit':
+      colorClass = S.inheritButton;
+  }
+
+  switch (size) {
+    case 'web1':
+      sizeClass = S.webButton1;
+      break;
+    case 'web2':
+      sizeClass = S.webButton2;
+      break;
+    case 'web3':
+      sizeClass = S.webButton3;
+      break;
+    case 'web4':
+      sizeClass = S.webButton4;
+      break;
+    case 'mobile1':
+      sizeClass = S.mobileButton1;
+      break;
+    case 'mobile2':
+      sizeClass = S.mobileButton2;
+  }
+
   return (
     <button
-      className={`${S.textButton} ${style}`}
+      className={`${S.textButton} ${colorClass} ${sizeClass}`}
       disabled={!isActive}
       onClick={onClick}
     >
