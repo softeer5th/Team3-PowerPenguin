883d12927cc06fafe390f20c8ea00bcbf503ade4
uri010 <dbfl0461@gmail.com>
2025-02-02 17:17:25 +0900

    Feat: OAuth 설정 값 파싱을 위한 OAuthProvider, OAuthConfig 추가 #11
    - OAuthProvider: OAuth 설정값 저장
    - OAuthConfig: application.properties 값을 provider에 따라 적절한 설정 값을 적용해 OAuthProvider로 매핑하여 관리

---
+++ back-end/reacton/src/main/java/com/softeer/reacton/global/oauth/OAuthConfig.java
@@ -0,0 +1,31 @@
+package com.softeer.reacton.global.oauth;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OAuthConfig {
+
+    @Value("${oauth.google.login-url}")
+    private String googleLoginUrl;
+    @Value("${oauth.google.client-id}")
+    private String googleClientId;
+    @Value("${oauth.google.client-secret}")
+    private String googleClientSecret;
+    @Value("${oauth.google.redirect-uri}")
+    private String googleRedirectUri;
+    @Value("${oauth.google.token-uri}")
+    private String googleTokenUri;
+    @Value("${oauth.google.user-info-uri}")
+    private String googleUserInfoUri;
+    @Value("${oauth.google.scope}")
+    private String googleScope;
+
+
+    public OAuthProvider getProvider(String provider) {
+        if (provider.equalsIgnoreCase("google")) {
+            return new OAuthProvider(googleLoginUrl, googleClientId, googleClientSecret, googleRedirectUri, googleTokenUri, googleUserInfoUri, googleScope);
+        }
+        throw new IllegalArgumentException("지원되지 않는 OAuth 제공자: " + provider);
+    }
+}
