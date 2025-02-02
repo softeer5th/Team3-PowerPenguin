12519c098e2404ab30050c761d3329a9deb28ac0
sunohkim <sunoh1222@gmail.com>
2025-02-02 16:35:58 +0900

    Feat: Professor 정보를 database에 접근해 저장하는 기능 구현 #11
    - oauthId, email, name 정보를 바탕으로 Professor database에 접근해 아이템 추가

--- back-end/reacton/src/main/java/com/softeer/reacton/domain/professor/ProfessorService.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/professor/ProfessorService.java
@@ -2,6 +2,28 @@ package com.softeer.reacton.domain.professor;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ProfessorService {
+
+    private final ProfessorRepository professorRepository;
+
+    public ProfessorService(ProfessorRepository professorRepository) {
+        this.professorRepository = professorRepository;
+    }
+
+    public Optional<Professor> findByOauthId(String oauthId) {
+        return professorRepository.findByOauthId(oauthId);
+    }
+
+    public void save(String oauthId, String email, String name) {
+        Professor professor = new Professor();
+
+        professor.setOauthId(oauthId);
+        professor.setEmail(email);
+        professor.setName(name);
+
+        professorRepository.save(professor);
+    }
 }
