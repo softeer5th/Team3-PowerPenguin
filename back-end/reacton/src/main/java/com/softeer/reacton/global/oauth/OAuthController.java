f8e0ebeda1895dac1e1a6355716e6a23499711d0
uri010 <dbfl0461@gmail.com>
2025-02-02 21:50:29 +0900

    Refactor: ResponseEntity로 응답 처리 통일 #11
    - HttpServletResponse 사용 제거 및 ResponseEntity로 변경

--- back-end/reacton/src/main/java/com/softeer/reacton/global/oauth/OAuthController.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/global/oauth/OAuthController.java
@@ -1,14 +1,13 @@
 package com.softeer.reacton.global.oauth;
 
 import com.softeer.reacton.global.oauth.dto.LoginResponse;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/auth")
@@ -18,24 +17,28 @@ public class OAuthController {
     private final OAuthService oauthService;
 
     @GetMapping("/{provider}/url")
-    public void getOauthLoginUrl(HttpServletResponse response, @PathVariable String provider) throws IOException {
-        response.sendRedirect(oauthService.getOauthLoginUrl(provider));
+    public ResponseEntity<Void> getOauthLoginUrl(@PathVariable String provider) {
+        String oauthLoginUrl = oauthService.getOauthLoginUrl(provider);
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .header(HttpHeaders.LOCATION, oauthLoginUrl)
+                .build();
     }
 
     @GetMapping("/{provider}/callback")
-    public ResponseEntity<LoginResponse> oauthCallback(HttpServletResponse response, @PathVariable String provider, @RequestParam String code) {
+    public ResponseEntity<LoginResponse> oauthCallback(@PathVariable String provider, @RequestParam String code) {
         LoginResponse loginResponse = oauthService.processOauthLogin(provider, code);
 
         ResponseCookie jwtCookie = ResponseCookie.from("access_token", loginResponse.getAccessToken())
                 .httpOnly(true)
-                .secure(false) // HTTP에서도 쿠키 전송 가능하도록 설정 (배포 환경에서는 true로 변경)
+                .secure(false) // TODO : HTTP에서도 쿠키 전송 가능하도록 설정 (배포 환경에서는 true로 변경)
                 .path("/")
                 .maxAge(60 * 60 * 24)
                 .sameSite("Strict")
                 .build();
-        response.addHeader(HttpHeaders.SET_COOKIE, jwtCookie.toString());
 
         // TODO : 프론트 리다이렉트 코드 추가 예정
-        return ResponseEntity.ok(loginResponse);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .body(loginResponse);
     }
 }
