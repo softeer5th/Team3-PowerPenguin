a34cf2f1b3aaea23f77819215fab8e77bba6bb78
uri010 <dbfl0461@gmail.com>
2025-02-03 14:32:06 +0900

    Feat: 회원가입 API 추가 #27
    - 쿠키에서 JWT 토큰의 isSignedUp 값 확인
      - true인 경우 회원가입 요청을 예외 처리
      - false인 경우 DB에 사용자 정보 존재 여부 확인 후 중복이면 예외 처리
    - 새로운 사용자 정보를 DB에 저장 후 201 CREATED 반환

--- back-end/reacton/src/main/java/com/softeer/reacton/domain/professor/ProfessorController.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/professor/ProfessorController.java
@@ -1,7 +1,45 @@
 package com.softeer.reacton.domain.professor;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping("/professors")
+@RequiredArgsConstructor
 public class ProfessorController {
+
+    private final ProfessorService professorService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signUp(
+            @RequestParam("name") String name,
+            @RequestPart(value = "profileImage", required = false) MultipartFile profileImageFile,
+            HttpServletRequest request) {
+        String oauthId = (String) request.getAttribute("oauthId");
+        String email = (String) request.getAttribute("email");
+        boolean isSignedUp = (boolean) request.getAttribute("isSignedUp");
+
+        String newAccessToken = professorService.signUp(name, profileImageFile, oauthId, email, isSignedUp);
+        ResponseCookie jwtCookie = ResponseCookie.from("access_token", newAccessToken)
+                .httpOnly(true)
+                .secure(false) // TODO : HTTP에서도 쿠키 전송 가능하도록 설정 (배포 환경에서는 true로 변경)
+                .path("/")
+                .maxAge(60 * 60 * 24)
+                .sameSite("Strict")
+                .build();
+
+        // TODO : 프론트 리다이렉트 코드 추가 예정
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .build();
+    }
+
 }
