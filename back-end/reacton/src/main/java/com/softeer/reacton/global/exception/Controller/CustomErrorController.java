314b34c011cf2409a751c505ec4a48c8b5e0c1bd
sunohkim <sunoh1222@gmail.com>
2025-02-04 16:13:35 +0900

    Feat: 404 Error에 대한 처리 기능 구현 #32
    - CustomErrorController 내에 잘못된 경로에 대한 예외 처리 기능 구현
    - Controller 외부에서 500 에러가 발생했을 경우에 대한 처리 기능 구현

---
+++ back-end/reacton/src/main/java/com/softeer/reacton/global/exception/Controller/CustomErrorController.java
@@ -0,0 +1,33 @@
+package com.softeer.reacton.global.exception.Controller;
+
+import com.softeer.reacton.global.exception.code.GlobalErrorCode;
+import com.softeer.reacton.global.exception.ExceptionResponse;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public ResponseEntity<ExceptionResponse> handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        System.out.println("status: " + status);
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return ResponseEntity
+                        .status(GlobalErrorCode.INVALID_PATH.getStatus())
+                        .body(ExceptionResponse.of(GlobalErrorCode.INVALID_PATH));
+            }
+        }
+        return ResponseEntity
+                .status(GlobalErrorCode.SERVER_ERROR.getStatus())
+                .body(ExceptionResponse.of(GlobalErrorCode.SERVER_ERROR));
+    }
+}
