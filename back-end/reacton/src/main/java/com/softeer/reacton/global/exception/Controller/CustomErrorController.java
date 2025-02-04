b0d64aff72d092f7826d40946ea9ab234d013f07
sunohkim <sunoh1222@gmail.com>
2025-02-04 19:29:28 +0900

    Style: 불필요한 출력 구문 제거 #32

--- back-end/reacton/src/main/java/com/softeer/reacton/global/exception/Controller/CustomErrorController.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/global/exception/Controller/CustomErrorController.java
@@ -17,8 +17,6 @@ public class CustomErrorController implements ErrorController {
     public ResponseEntity<ExceptionResponse> handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
-        System.out.println("type: " + status.getClass().getName());
-
         if (status != null) {
             int statusCode = Integer.parseInt(status.toString());
             if (statusCode == HttpStatus.NOT_FOUND.value()) {
