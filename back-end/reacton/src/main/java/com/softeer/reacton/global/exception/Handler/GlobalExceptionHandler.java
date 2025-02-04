1ea7549edf4076af5b42433f4a447e8331b4968f
sunohkim <sunoh1222@gmail.com>
2025-02-04 16:20:38 +0900

    Style: 사용하지 않는 import 문 제거 #32

--- back-end/reacton/src/main/java/com/softeer/reacton/global/exception/Handler/GlobalExceptionHandler.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/global/exception/Handler/GlobalExceptionHandler.java
@@ -9,7 +9,6 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.MethodNotAllowedException;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
