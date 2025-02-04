9f89d6e8c3b4cbfae027f0c85b3b369282ba6afb
sunohkim <sunoh1222@gmail.com>
2025-02-04 16:34:35 +0900

    Style: Error message에 누락된 period 추가 #32

--- back-end/reacton/src/main/java/com/softeer/reacton/global/exception/code/JwtErrorCode.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/global/exception/code/JwtErrorCode.java
@@ -14,7 +14,7 @@ public enum JwtErrorCode implements ErrorCode {
     INVALID_SIGNATURE("JWT 토큰 서명이 올바르지 않습니다.", HttpStatus.UNAUTHORIZED),
     MALFORMED_SIGNATURE("JWT 토큰 형식이 올바르지 않습니다.", HttpStatus.UNAUTHORIZED),
     TOKEN_PARSING_FAILED("JWT 토큰을 파싱하는 중 오류가 발생했습니다.", HttpStatus.UNAUTHORIZED),
-    MISSING_CLAIM("JWT 토큰의 필수 정보가 누락되었습니다", HttpStatus.UNAUTHORIZED);
+    MISSING_CLAIM("JWT 토큰의 필수 정보가 누락되었습니다.", HttpStatus.UNAUTHORIZED);
 
     private final String message;
     private final HttpStatus status;
