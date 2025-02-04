c763ae33fc9f6cc4e1d3b077f804a8f1aed8abc4
sunohkim <sunoh1222@gmail.com>
2025-02-04 16:00:54 +0900

    Feat: Controller에서 발생한 예외에 대한 커스텀 handler 구현 #32
    - Controller에서 발생한 예외에 대해 자체 handler로 처리
    - 사용자 정의 예외, 공통 발생 예외에 대한 처리 가능

---
+++ back-end/reacton/src/main/java/com/softeer/reacton/global/exception/Handler/GlobalExceptionHandler.java
@@ -0,0 +1,58 @@
+package com.softeer.reacton.global.exception.Handler;
+
+import com.softeer.reacton.global.exception.BaseException;
+import com.softeer.reacton.global.exception.ExceptionResponse;
+import com.softeer.reacton.global.exception.code.GlobalErrorCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.MethodNotAllowedException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ExceptionResponse> handleBaseException(BaseException e) {
+        return ResponseEntity
+                .status(e.getErrorCode().getStatus())
+                .body(ExceptionResponse.of(e.getErrorCode()));
+    }
+
+    @ExceptionHandler(MethodNotAllowedException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodNotAllowedException(MethodNotAllowedException e) {
+        return ResponseEntity
+                .status(GlobalErrorCode.METHOD_NOT_ALLOWED.getStatus())
+                .body(ExceptionResponse.of(GlobalErrorCode.METHOD_NOT_ALLOWED));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ExceptionResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        return ResponseEntity
+                .status(GlobalErrorCode.METHOD_NOT_ALLOWED.getStatus())
+                .body(ExceptionResponse.of(GlobalErrorCode.METHOD_NOT_ALLOWED));
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<ExceptionResponse> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
+        return ResponseEntity
+                .status(GlobalErrorCode.UNSUPPORTED_MEDIA_TYPE.getStatus())
+                .body(ExceptionResponse.of(GlobalErrorCode.UNSUPPORTED_MEDIA_TYPE));
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ExceptionResponse> handleNoHandlerFoundException(NoHandlerFoundException e) {
+        return ResponseEntity
+                .status(GlobalErrorCode.INVALID_PATH.getStatus())
+                .body(ExceptionResponse.of(GlobalErrorCode.INVALID_PATH));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleUnexpectedException(Exception e) {
+        return ResponseEntity
+                .status(GlobalErrorCode.SERVER_ERROR.getStatus())
+                .body(ExceptionResponse.of(GlobalErrorCode.SERVER_ERROR));
+    }
+}
