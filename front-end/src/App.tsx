3ad65b2b5b66b1ac4cfb90c7588e24eb250d4eff
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-02-05 15:27:22 +0900

    Feat: 프론트엔드 라우팅 설정 #41
    - 라우팅 포인트 생성
    - 라우터 구조에 따른 페이지 디렉토리 구조 설정

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,5 +1,30 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="professor">
+          <Route index element={<ProfessorHomeLayout />}>
+            <Route index element={<ProfessorHome />} />
+            <Route path="search" element={<ProfessorSearch />} />
+          </Route>
+          <Route path="login" element={<ProfessorLogin />} />
+          <Route path="register" element={<ProfessorRegister />} />
+          <Route path="profile" element={<ProfessorProfile />} />
+          <Route path="course/:id">
+            <Route index element={<ProfessorCourse />} />
+            <Route path="classroom" element={<ProfessorClassRoom />} />
+          </Route>
+        </Route>
+        <Route path="student">
+          <Route index element={<StudentHome />} />
+          <Route path="course/:id" element={<StudentCourse />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
