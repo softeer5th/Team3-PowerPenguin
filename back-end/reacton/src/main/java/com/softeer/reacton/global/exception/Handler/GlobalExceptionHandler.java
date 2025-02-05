c34012ddc22c9bf4788e78c9047200c2f0fa27e1
sunohkim <sunoh1222@gmail.com>
2025-02-05 15:08:45 +0900

    Feat: global 예외 케이스 추가 #32
    - DTO의 유효성 검사에 실패한 경우
    - 요청에 대한 필수 파라미터가 누락된 경우
    - 요청 파라미터의 타입이 일치하지 않는 경우

--- back-end/reacton/src/main/java/com/softeer/reacton/global/exception/Handler/GlobalExceptionHandler.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/global/exception/Handler/GlobalExceptionHandler.java
@@ -4,9 +4,12 @@ import com.softeer.reacton.global.exception.BaseException;
 import com.softeer.reacton.global.exception.ExceptionResponse;
 import com.softeer.reacton.global.exception.code.GlobalErrorCode;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.MethodNotAllowedException;
@@ -45,5 +48,29 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(GlobalErrorCode.UNSUPPORTED_MEDIA_TYPE.getStatus())
                 .body(ExceptionResponse.of(GlobalErrorCode.UNSUPPORTED_MEDIA_TYPE));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleValidationException(MethodArgumentNotValidException e) {
+        log.warn(GlobalErrorCode.VALIDATION_FAIL.getMessage());
+        return ResponseEntity
+                .status(GlobalErrorCode.VALIDATION_FAIL.getStatus())
+                .body(ExceptionResponse.of(GlobalErrorCode.VALIDATION_FAIL));
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ExceptionResponse> handleMissingParamException(MissingServletRequestParameterException e) {
+        log.warn(GlobalErrorCode.MISSING_PARAMETER.getMessage());
+        return ResponseEntity
+                .status(GlobalErrorCode.MISSING_PARAMETER.getStatus())
+                .body(ExceptionResponse.of(GlobalErrorCode.MISSING_PARAMETER));
+    }
+
+    @ExceptionHandler(TypeMismatchException.class)
+    public ResponseEntity<ExceptionResponse> handleTypeMismatchException(TypeMismatchException e) {
+        log.warn(GlobalErrorCode.WRONG_TYPE.getMessage());
+        return ResponseEntity
+                .status(GlobalErrorCode.WRONG_TYPE.getStatus())
+                .body(ExceptionResponse.of(GlobalErrorCode.WRONG_TYPE));
     }
 }
