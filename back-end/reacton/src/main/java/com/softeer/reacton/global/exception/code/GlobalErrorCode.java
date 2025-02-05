eee0d38ef9188a3d5b260f933ceea98886e2ff84
sunohkim <sunoh1222@gmail.com>
2025-02-05 14:32:08 +0900

    Chore: 에러 메시지 내용 일부 수정 #32
    - 일관성을 위해 에러가 발생한 문제 상황을 명시하도록 수정
    - 오타 수정

--- back-end/reacton/src/main/java/com/softeer/reacton/global/exception/code/GlobalErrorCode.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/global/exception/code/GlobalErrorCode.java
@@ -10,8 +10,8 @@ public enum GlobalErrorCode implements ErrorCode {
     INVALID_PATH("유효하지 않은 경로입니다.", HttpStatus.NOT_FOUND),
     SERVER_ERROR("서버와의 연결에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     BAD_REQUEST("잘못된 요청입니다.", HttpStatus.BAD_REQUEST),
-    UNAUTHORIZED("인증이 필요합니다.", HttpStatus.UNAUTHORIZED),
-    FORBIDDEN("접근 권한이 업습니다.", HttpStatus.FORBIDDEN),
+    UNAUTHORIZED("인증되지 않은 접근입니다.", HttpStatus.UNAUTHORIZED),
+    FORBIDDEN("접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
     METHOD_NOT_ALLOWED("허용되지 않은 HTTP 메서드입니다.", HttpStatus.METHOD_NOT_ALLOWED),
     UNSUPPORTED_MEDIA_TYPE("지원하지 않는 미디어 타입입니다.", HttpStatus.UNSUPPORTED_MEDIA_TYPE),
     TOO_MANU_REQUEST("요청이 너무 많습니다. 잠시 후 다시 시도해 주세요.", HttpStatus.TOO_MANY_REQUESTS);
