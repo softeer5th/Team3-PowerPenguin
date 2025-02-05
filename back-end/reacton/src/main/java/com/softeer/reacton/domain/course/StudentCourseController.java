535cdb524a9323f34a75cf9e83ae8248a6d94e41
sunohkim <sunoh1222@gmail.com>
2025-02-05 18:19:52 +0900

    Feat: Swagger 태그 추가 #40

--- back-end/reacton/src/main/java/com/softeer/reacton/domain/course/StudentCourseController.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/course/StudentCourseController.java
@@ -1,7 +1,9 @@
 package com.softeer.reacton.domain.course;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Student API", description = "학생 사용자 관련 API")
 public class StudentCourseController {
 }
