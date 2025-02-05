4b919b2e19a31089a1fc68b074b61dca8b1cf056
sunohkim <sunoh1222@gmail.com>
2025-02-05 22:09:45 +0900

    Feat: 기존 구현된 API에 Swagger 연동 #40
    - API별 이름과 description 추가
    - 예상되는 결과 목록 추가

--- back-end/reacton/src/main/java/com/softeer/reacton/global/exception/Controller/CustomErrorController.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/global/exception/Controller/CustomErrorController.java
@@ -1,7 +1,10 @@
 package com.softeer.reacton.global.exception.Controller;
 
 import com.softeer.reacton.global.exception.code.GlobalErrorCode;
-import com.softeer.reacton.global.exception.ExceptionResponse;
+import com.softeer.reacton.global.DTO.FailResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.log4j.Log4j2;
@@ -15,10 +18,19 @@ import java.util.Optional;
 
 @Log4j2
 @RestController
+@Tag(name = "CustomError API", description = "에러 처리 관련 API")
 public class CustomErrorController implements ErrorController {
 
     @RequestMapping("/error")
-    public ResponseEntity<ExceptionResponse> handleError(HttpServletRequest request) {
+    @Operation(
+            summary = "에러 처리",
+            description = "Dispatcher Sublet에 의해 넘겨진 에러에 대한 처리를 수행합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "404", description = "유효하지 않은 경로입니다."),
+                    @ApiResponse(responseCode = "500", description = "서버와의 연결에 실패했습니다.")
+            }
+    )
+    public ResponseEntity<FailResponse> handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
         Integer statusCode = Optional.ofNullable(status)
@@ -30,12 +42,12 @@ public class CustomErrorController implements ErrorController {
             log.warn(GlobalErrorCode.INVALID_PATH.getMessage());
             return ResponseEntity
                     .status(GlobalErrorCode.INVALID_PATH.getStatus())
-                    .body(ExceptionResponse.of(GlobalErrorCode.INVALID_PATH));
+                    .body(FailResponse.of(GlobalErrorCode.INVALID_PATH));
         }
 
         log.error(GlobalErrorCode.SERVER_ERROR.getMessage());
         return ResponseEntity
                 .status(GlobalErrorCode.SERVER_ERROR.getStatus())
-                .body(ExceptionResponse.of(GlobalErrorCode.SERVER_ERROR));
+                .body(FailResponse.of(GlobalErrorCode.SERVER_ERROR));
     }
 }
