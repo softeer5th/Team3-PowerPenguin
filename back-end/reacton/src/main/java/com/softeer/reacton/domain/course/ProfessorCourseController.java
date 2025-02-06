954b34da4dc1945428f332b3c9235ec44dbc7aad
uri010 <dbfl0461@gmail.com>
2025-02-06 18:45:25 +0900

    Feat: 교수 수업 생성 API 구현 #61
    - 교수의 수업을 생성하는 API 추가
    - 랜덤한 6자리 입장 코드(accessCode) 생성시 SecureRandom 이용
    - Course 및 Schedule 엔티티 저장 로직 구현
    - 수업 생성 과정의 디버깅 로그 추가

--- back-end/reacton/src/main/java/com/softeer/reacton/domain/course/ProfessorCourseController.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/course/ProfessorCourseController.java
@@ -1,7 +1,48 @@
 package com.softeer.reacton.domain.course;
 
+import com.softeer.reacton.domain.course.dto.CourseCreateRequest;
+import com.softeer.reacton.global.dto.SuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @RestController
+@RequestMapping("/professors/courses")
+@Tag(name = "Professor Course API", description = "교수 수업 관련 API")
+@RequiredArgsConstructor
 public class ProfessorCourseController {
+    private final ProfessorCourseService professorCourseService;
+
+    @PostMapping
+    @Operation(
+            summary = "수업 생성 요청",
+            description = "수업 데이터를 받아 데이터베이스 저장하고 courseId를 반환합니다.",
+            responses = {@ApiResponse(responseCode = "201", description = "수업이 생성되었습니다.")}
+    )
+    public ResponseEntity<SuccessResponse<Map<String, String>>> createCourse(HttpServletRequest request, @RequestBody CourseCreateRequest courseCreateRequest) {
+        String oauthId = (String) request.getAttribute("oauthId");
+
+        long courseId = professorCourseService.createCourse(oauthId, courseCreateRequest);
+        log.info("수업을 생성하고 DB에 저장했습니다. : courseId = {}", courseId);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("courseId", String.valueOf(courseId));
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(SuccessResponse.of("수업이 생성되었습니다.", response));
+    }
 }
--- back-end/reacton/src/main/java/com/softeer/reacton/domain/course/ProfessorCourseService.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/course/ProfessorCourseService.java
@@ -1,7 +1,49 @@
 package com.softeer.reacton.domain.course;
 
+import com.softeer.reacton.domain.course.dto.CourseCreateRequest;
+import com.softeer.reacton.domain.professor.Professor;
+import com.softeer.reacton.domain.professor.ProfessorRepository;
+import com.softeer.reacton.domain.schedule.Schedule;
+import com.softeer.reacton.global.exception.BaseException;
+import com.softeer.reacton.global.exception.code.ProfessorErrorCode;
+import com.softeer.reacton.global.util.TimeUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class ProfessorCourseService {
+    private final ProfessorRepository professorRepository;
+    private final CourseRepository courseRepository;
+
+    public long createCourse(String oauthId, CourseCreateRequest request) {
+        log.debug("수업을 생성합니다.");
+
+        Professor professor = professorRepository.findByOauthId(oauthId)
+                .orElseThrow(() -> new BaseException(ProfessorErrorCode.PROFESSOR_NOT_FOUND));
+
+        SecureRandom secureRandom = new SecureRandom();
+        int accessCode = 100000 + secureRandom.nextInt(900000);
+        log.debug("입장코드용 랜덤한 6자리 숫자를 생성합니다 : accessCode = {}", accessCode);
+
+        Course course = Course.of(request, accessCode, professor);
+        List<Schedule> schedules = request.getSchedules().stream()
+                .map(scheduleRequest ->
+                        Schedule.builder()
+                                .day(scheduleRequest.getDay())
+                                .startTime(TimeUtil.parseTime(scheduleRequest.getStartTime()))
+                                .endTime(TimeUtil.parseTime(scheduleRequest.getEndTime()))
+                                .course(course)
+                                .build())
+                .collect(Collectors.toList());
+        course.setSchedules(schedules);
+
+        return courseRepository.save(course).getId();
+    }
 }
