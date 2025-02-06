3ae0545488ea356c7875b7a56a12a9c06fce8fbd
sunohkim <sunoh1222@gmail.com>
2025-02-06 17:00:39 +0900

    Feat: 성공적인 응답 DTO 생성 #58
    - 내용: success, message, data

--- back-end/reacton/src/main/java/com/softeer/reacton/global/dto/SuccessResponse.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/global/dto/SuccessResponse.java
@@ -1,10 +1,27 @@
 package com.softeer.reacton.global.dto;
 
+import lombok.Builder;
 import lombok.Getter;
+
+import java.util.Map;
 
 @Getter
 public class SuccessResponse<T> {
-    private boolean success;
-    private String message;
-    private T data;
+    private final boolean success;
+    private final String message;
+    private final T data;
+
+    @Builder
+    public SuccessResponse(String message, T data) {
+        this.success = true;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static SuccessResponse<Map<String, String>> of(String message, Map<String, String> data) {
+        return SuccessResponse.<Map<String, String>>builder()
+                .message(message)
+                .data(data)
+                .build();
+    }
 }
