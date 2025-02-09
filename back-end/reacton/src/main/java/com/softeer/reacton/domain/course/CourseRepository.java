8835cdfc53834551e61d2de3c39b8f0f9bffbe75
sunohkim <sunoh1222@gmail.com>
2025-02-09 18:26:03 +0900

    Feat: Course 관련 DB에 접근하는 기능 추가 #76
    - Getter 어노테이션으로 접근하는 기능
    - accessCode로 수업을 조회하는 기능

--- back-end/reacton/src/main/java/com/softeer/reacton/domain/course/CourseRepository.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/course/CourseRepository.java
@@ -2,5 +2,8 @@ package com.softeer.reacton.domain.course;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    Optional<Course> findByAccessCode(int accessCode);
 }
