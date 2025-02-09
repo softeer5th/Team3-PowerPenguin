606ad6af4047872ab99c08cf587f2ffa8dadf9a5
uri010 <dbfl0461@gmail.com>
2025-02-09 22:49:19 +0900

    Feat: 수업 저장 로직만 담당하는 메서드를 새로운 클래스에 분리해 트랜잭션 따로 적용 #73

---
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/course/ProfessorCourseTransactionService.java
@@ -0,0 +1,23 @@
+package com.softeer.reacton.domain.course;
+
+import com.softeer.reacton.domain.course.dto.CourseRequest;
+import com.softeer.reacton.domain.professor.Professor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProfessorCourseTransactionService {
+
+    private final CourseRepository courseRepository;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public long saveCourseWithNewTransaction(CourseRequest request, Professor professor, String accessCode) {
+        Course course = Course.create(request, accessCode, professor);
+        return courseRepository.save(course).getId();
+    }
+}
