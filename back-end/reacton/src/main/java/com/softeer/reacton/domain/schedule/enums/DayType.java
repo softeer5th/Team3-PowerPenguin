a15c366077ed03b02824faca8122be7175104363
sunohkim <sunoh1222@gmail.com>
2025-02-09 22:25:09 +0900

    Chore: Annotation을 활용해 불필요한 코드 제거 #76
    - @Getter, @AllArgsConstructor 추가

--- back-end/reacton/src/main/java/com/softeer/reacton/domain/schedule/enums/DayType.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/schedule/enums/DayType.java
@@ -1,17 +1,14 @@
 package com.softeer.reacton.domain.schedule.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum DayType {
     월(1), 화(2), 수(3), 목(4), 금(5), 토(6), 일(7);
 
     private final int order;
-
-    DayType(int order) {
-        this.order = order;
-    }
-
-    public int getOrder() {
-        return order;
-    }
 
     public static int getDayOrder(String day) {
         for (DayType d : values()) {
