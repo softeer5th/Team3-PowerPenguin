4bd3789620c770d061fc2b9fb7bd5dd413ccbb76
sunohkim <sunoh1222@gmail.com>
2025-02-10 16:04:27 +0900

    Feat: 정렬 순서를 지정하여 원하는 객체 List를 가져올 수 있는 메서드 추가 #74
    - JPA는 MySQL에서 지정한 ENUM 옵션을 처리하지 못하는 점을 고려, JPA @Query에서 자체적으로 정렬 기준을 설정하도록 기능 추가

--- back-end/reacton/src/main/java/com/softeer/reacton/domain/question/QuestionRepository.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/question/QuestionRepository.java
@@ -2,5 +2,8 @@ package com.softeer.reacton.domain.question;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    List<Question> findAllByIdOrderByCreatedAtAsc(Long id);
 }
--- back-end/reacton/src/main/java/com/softeer/reacton/domain/request/RequestRepository.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/request/RequestRepository.java
@@ -2,5 +2,8 @@ package com.softeer.reacton.domain.request;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RequestRepository extends JpaRepository<Request, Long> {
+    List<Request> findAllByIdOrderByCountDesc(Long id);
 }
