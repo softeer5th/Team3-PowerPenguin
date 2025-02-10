1223e17dcffd131e13a1f61bda4e4c2a61b0d95a
sunohkim <sunoh1222@gmail.com>
2025-02-10 16:30:28 +0900

    Fix: Schedule 정렬 방식 수정 #76
    - JPA에서 옵션을 통해 Schedule의 정렬 순서를 지정하도록 수정

--- back-end/reacton/src/main/java/com/softeer/reacton/domain/schedule/ScheduleRepository.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/schedule/ScheduleRepository.java
@@ -1,6 +1,13 @@
 package com.softeer.reacton.domain.schedule;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    @Query("SELECT s FROM Schedule s WHERE s.course.id = :courseId " +
+            "ORDER BY FIELD(s.day, '월', '화', '수', '목', '금', '토', '일'), s.startTime ASC")
+    List<Schedule> findSchedulesByCourseId(@Param("courseId") Long courseId);
 }
