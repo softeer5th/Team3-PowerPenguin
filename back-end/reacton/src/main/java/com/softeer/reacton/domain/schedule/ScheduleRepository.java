a4fb084dafda10e0f5ff902794ec8b080db6981f
sunohkim <sunoh1222@gmail.com>
2025-02-10 16:57:51 +0900

    Chore: Schedule 정렬 관련 기능 수정 #74
    - FIELD는 다른 데이터베이스와 호환이 되지 않아, 호환성을 위해 CASE 문을 통해 정렬 순서 지정

--- back-end/reacton/src/main/java/com/softeer/reacton/domain/schedule/ScheduleRepository.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/schedule/ScheduleRepository.java
@@ -8,6 +8,14 @@ import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     @Query("SELECT s FROM Schedule s WHERE s.course.id = :courseId " +
-            "ORDER BY FIELD(s.day, '월', '화', '수', '목', '금', '토', '일'), s.startTime ASC")
+            "ORDER BY CASE s.day " +
+            "WHEN '월' THEN 1 " +
+            "WHEN '화' THEN 2 " +
+            "WHEN '수' THEN 3 " +
+            "WHEN '목' THEN 4 " +
+            "WHEN '금' THEN 5 " +
+            "WHEN '토' THEN 6 " +
+            "WHEN '일' THEN 7 " +
+            "ELSE 8 END, s.startTime ASC")
     List<Schedule> findSchedulesByCourseId(@Param("courseId") Long courseId);
 }
