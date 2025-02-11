17146c5db9e67f402e64d963881addbf33295647
kimdongyoung <wwweric12@naver.com>
2025-02-11 15:49:42 +0900

    Feat: student react 페이지 구현 #88 - 모바일, 스플릿, 웹 뷰로 반응형 구현 - 각각의 card를 리스트로 만들어서 props로 넘겨서 구현

---
+++ front-end/src/pages/student/course/react/StudentReact.tsx
@@ -0,0 +1,61 @@
+import S from './StudentReact.module.css';
+import OkaySvg from '../../../../assets/icons/okay-emoji.svg?react';
+import ClapSvg from '../../../../assets/icons/clap-emoji.svg?react';
+import ThumbUpSvg from '../../../../assets/icons/thumb-up-emoji.svg?react';
+import HeartSvg from '../../../../assets/icons/heart-emoji.svg?react';
+import CrySvg from '../../../../assets/icons/cry-emoji.svg?react';
+import ScreamSvg from '../../../../assets/icons/scream-emoji.svg?react';
+import SuccessPopup from '../components/SuccessPopup';
+import { useState } from 'react';
+import ReactCard from './components/ReactCard';
+import { classroomRepository } from '../../../../di';
+import { Reaction } from '../../../../core/model';
+
+const CARD_List = [
+  { type: 'okay', icon: OkaySvg },
+  { type: 'clap', icon: ClapSvg },
+  { type: 'thumb', icon: ThumbUpSvg },
+  { type: 'scream', icon: ScreamSvg },
+  { type: 'cry', icon: CrySvg },
+  { type: 'like', icon: HeartSvg },
+] as const;
+
+const StudentReact = ({ courseId }: { courseId: string }) => {
+  const [successPopup, setSuccessPopup] = useState<boolean>(false);
+
+  const handleCardClick = async (reaction: Reaction) => {
+    try {
+      await classroomRepository.sendReaction(courseId, reaction);
+      setSuccessPopup(true);
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  };
+  return (
+    <div className={S.pageLayout}>
+      <div className={S.reactTitle}>
+        <span className={S.titleStrong}>간단한 반응</span>을 남기며 소통해보세요
+      </div>
+      <div className={S.reactDesc}>이모지로 가볍게 감정을 표현할 수 있어요</div>
+      <div className={S.cardContainer}>
+        {CARD_List.map((item) => (
+          <ReactCard
+            key={item.type}
+            Icon={item.icon}
+            onCardClick={() => handleCardClick(item.type)}
+          />
+        ))}
+      </div>
+      {successPopup && (
+        <SuccessPopup
+          text="라이브 피드백 전송 성공"
+          onClose={() => setSuccessPopup(false)}
+        />
+      )}
+    </div>
+  );
+};
+
+export default StudentReact;
