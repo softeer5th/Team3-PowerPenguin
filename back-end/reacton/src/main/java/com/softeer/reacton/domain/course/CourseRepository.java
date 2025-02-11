db532e50a8b45621e9ebaf2f31ff2e6fefe06427
sunohkim <sunoh1222@gmail.com>
2025-02-11 12:03:24 +0900

    Feat: Course database 조회 기능 추가 #74
    - findCoursesByDayAndProfessor: 현재 날짜, 교수와 일치하는 수업을 현재 날짜 수업 시작 시간 순으로 정렬
    - findCoursesByProfessor : 교수와 일치하는 수업을 Course 생성 시간의 역순으로 정렬
    - Fetch Join을 사용해 쿼리 최적화 도입

--- back-end/reacton/src/main/java/com/softeer/reacton/domain/course/CourseRepository.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/course/CourseRepository.java
@@ -1,6 +1,23 @@
 package com.softeer.reacton.domain.course;
 
+import com.softeer.reacton.domain.professor.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    @Query("SELECT DISTINCT c FROM Course c " +
+            "LEFT JOIN FETCH c.schedules s " +
+            "WHERE s.day = :day AND c.professor = :professor " +
+            "ORDER BY s.startTime ASC")
+    List<Course> findCoursesByDayAndProfessor(@Param("day") String day, @Param("professor") Professor professor);
+
+    @Query("SELECT DISTINCT c FROM Course c " +
+            "LEFT JOIN FETCH c.schedules s " +
+            "WHERE c.professor = :professor " +
+            "ORDER BY c.createdAt DESC")
+    List<Course> findCoursesByProfessor(@Param("professor") Professor professor);
 }
