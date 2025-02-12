a4e8f6679c29a4a6b005bcb9b5a52f29841c5951
sunohkim <sunoh1222@gmail.com>
2025-02-12 19:23:53 +0900

    Fix: 자동 Merge 과정에서 발생한 Conflict 이슈 해결 #107

--- back-end/reacton/src/main/java/com/softeer/reacton/domain/course/ProfessorCourseService.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/course/ProfessorCourseService.java
@@ -16,7 +16,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.security.SecureRandom;
 import java.time.LocalTime;
