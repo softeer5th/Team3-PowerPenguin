fcb79d1e4e1154ee4a3681c78ed565d75a77f8da
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-02-16 01:19:35 +0900

    Feat: 강의 반응 컴포넌트 구현 #128
    - 강의 반응 바 컴포넌트 구현
    - 강의 반응 통계 컴포넌트 구현

--- front-end/src/pages/professor/course/components/CourseRequest.tsx
+++ front-end/src/pages/professor/course/components/CourseRequest.tsx
@@ -1,12 +1,47 @@
+import S from './CourseRequest.module.css';
 import { Requests } from '@/core/model';
-import S from './CourseReaction.module.css';
+import BulbEmoji from '@/assets/icons/bulb-emoji.svg?react';
+import EarEmoji from '@/assets/icons/ear-emoji.svg?react';
+import MagnifierEmoji from '@/assets/icons/magnifier-emoji.svg?react';
+import WindEmoji from '@/assets/icons/wind-emoji.svg?react';
+import WishEmoji from '@/assets/icons/wish-emoji.svg?react';
+import RequestBar from './RequestBar';
 
 type CourseRequestProps = {
   requests: Requests | [];
 };
 
+const EmojiType = {
+  hard: WishEmoji,
+  fast: WindEmoji,
+  question: BulbEmoji,
+  size: MagnifierEmoji,
+  sound: EarEmoji,
+};
+
 const CourseRequest = ({ requests }: CourseRequestProps) => {
-  return <div>CourseReaction</div>;
+  const sortedRequests = requests.sort((a, b) => b.count - a.count);
+
+  return (
+    <div className={S.courseRequest}>
+      {sortedRequests.length === 0 ? (
+        <div className={S.requestBar}>
+          <p>No requests yet</p>
+        </div>
+      ) : (
+        sortedRequests.map((request, index) => (
+          <RequestBar
+            key={index}
+            index={index}
+            title={request.type.title}
+            Emoji={EmojiType[request.type.kind]}
+            count={request.count}
+            percentage={(request.count / sortedRequests[0].count) * 100}
+          />
+        ))
+      )}
+    </div>
+  );
 };
 
 export default CourseRequest;
---
+++ front-end/src/pages/professor/course/components/RequestBar.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import S from './RequestBar.module.css';
+
+type RequestBarProps = {
+  index: number;
+  title: string;
+  Emoji: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  count: number;
+  percentage: number;
+};
+
+type RequestBarContentProps = {
+  title: string;
+  Emoji: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  count: number;
+};
+
+const RequestBarContent = ({ title, Emoji, count }: RequestBarContentProps) => {
+  return (
+    <div className={S.content}>
+      <div className={S.main}>
+        <div className={S.emojiBackground}>
+          <Emoji className={S.emoji} />
+        </div>
+        <div className={S.title}>
+          <span className={S.titleGreen}>{title}</span>
+        </div>
+      </div>
+      <div className={S.count}>
+        <span>{count}</span>
+      </div>
+    </div>
+  );
+};
+
+const RequestBar = ({
+  index,
+  title,
+  Emoji,
+  count,
+  percentage,
+}: RequestBarProps) => {
+  const displayTitle = title.slice(0, -2);
+  return (
+    <div
+      className={`${S.requestBar} ${index === 0 ? S.first : ''} ${index === 1 ? S.second : ''}`}
+    >
+      <div
+        className={S.percentage}
+        style={{ width: percentage === 0 ? '18px' : `${percentage}%` }}
+      >
+        <RequestBarContent title={displayTitle} Emoji={Emoji} count={count} />
+      </div>
+      <RequestBarContent title={displayTitle} Emoji={Emoji} count={count} />
+    </div>
+  );
+};
+
+export default RequestBar;
