eec022a50df17b8b57c093c753848b4f39ad0733
sunohkim <sunoh1222@gmail.com>
2025-02-17 14:14:40 +0900

    Feat: 학생 인증 쿠키 생성 및 검증 #136
    - courseId, studentId 값을 바탕으로 JWT 토큰 형태의 쿠키 생성
    - filter에서 학생 관련 request일 경우 courseId, studentId의 유효성 검증
    - CookieConfig에 학생 인증 쿠키 만료 시간 86400초로 지정

--- back-end/reacton/src/main/java/com/softeer/reacton/global/config/CookieConfig.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/global/config/CookieConfig.java
@@ -9,10 +9,13 @@ import org.springframework.stereotype.Component;
 public class CookieConfig {
     private final int authExpiration;
     private final int signupExpiration;
+    private final int studentAccessExpiration;
 
     public CookieConfig(@Value("${cookie.auth.expiration}") int authExpiration,
-                        @Value("${cookie.signup.expiration}") int signupExpiration) {
+                        @Value("${cookie.signup.expiration}") int signupExpiration,
+                        @Value("${cookie.student-access.expiration}") int studentAccessExpiration) {
         this.authExpiration = authExpiration;
         this.signupExpiration = signupExpiration;
+        this.studentAccessExpiration = studentAccessExpiration;
     }
 }
--- back-end/reacton/src/main/java/com/softeer/reacton/global/jwt/JwtTokenUtil.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/global/jwt/JwtTokenUtil.java
@@ -21,16 +21,19 @@ public class JwtTokenUtil {
     private final Key secretKey;
     private final long authTokenExpiration;
     private final long signUpTokenExpiration;
+    private final long studentAccessTokenExpiration;
 
     public JwtTokenUtil(
             @Value("${jwt.secret-key}") String secretKey,
             @Value("${jwt.auth-access-token.expiration}") long authTokenExpiration,
-            @Value("${jwt.signup-access-token.expiration}") long signUpTokenExpiration) {
+            @Value("${jwt.signup-access-token.expiration}") long signUpTokenExpiration,
+            @Value("${jwt.student-access-token.expiration}") long studentAccessTokenExpiration) {
 
         String encodedKey = Base64.getEncoder().encodeToString(secretKey.getBytes(StandardCharsets.UTF_8));
         this.secretKey = Keys.hmacShaKeyFor(encodedKey.getBytes());
         this.authTokenExpiration = authTokenExpiration;
         this.signUpTokenExpiration = signUpTokenExpiration;
+        this.studentAccessTokenExpiration = studentAccessTokenExpiration;
     }
 
     public String createAuthAccessToken(String oauthId, String email) {
@@ -57,6 +60,17 @@ public class JwtTokenUtil {
                 .compact();
     }
 
+    public String createStudentAccessToken(String studentId, Long courseId) {
+        log.debug("학생 JWT 토큰을 생성합니다. : studentId = {}", studentId);
+
+        return Jwts.builder()
+                .claim("studentId", studentId)
+                .claim("courseId", courseId)
+                .setExpiration(new Date(System.currentTimeMillis() + studentAccessTokenExpiration))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
     public void validateToken(String token) {
         log.debug("JWT 토큰의 유효성을 검증합니다.");
 
@@ -75,12 +89,53 @@ public class JwtTokenUtil {
         log.debug("토큰 유효성 검증이 완료되었습니다.");
     }
 
-    public Map<String, Object> getUserInfoFromToken(String token) {
-        log.debug("토큰으로부터 사용자 정보를 가져옵니다.");
+    public Map<String, Object> getProfessorInfoFromToken(String token) {
+        log.debug("토큰으로부터 교수 정보를 가져옵니다.");
 
-        Claims claims;
+        Claims claims = getClaims(token);
+
+        String oauthId = claims.get("oauthId", String.class);
+        String email = claims.get("email", String.class);
+        Boolean isSignedUp = claims.get("isSignedUp", Boolean.class);
+
+        if (oauthId == null || email == null || isSignedUp == null) {
+            log.debug("JWT 토큰으로부터 교수 정보를 가져오는 과정에서 발생한 에러입니다. : Missing required claims in JWT token.");
+            throw new BaseException(JwtErrorCode.ACCESS_TOKEN_ERROR);
+        }
+
+        log.debug("교수 정보를 가져오는 데 성공했습니다. : oauthId = {}, email = {}, isSignedUp = {}", oauthId, email, isSignedUp);
+
+        return Map.of(
+                "oauthId", oauthId,
+                "email", email,
+                "isSignedUp", isSignedUp
+        );
+    }
+
+    public Map<String, Object> getStudentInfoFromToken(String token) {
+        log.debug("토큰으로부터 학생 정보를 가져옵니다.");
+
+        Claims claims = getClaims(token);
+
+        String studentId = claims.get("studentId", String.class);
+        Long courseId = claims.get("courseId", Long.class);
+
+        if (studentId == null || courseId == null) {
+            log.debug("JWT 토큰으로부터 학생 정보를 가져오는 과정에서 발생한 에러입니다. : Missing required claims in JWT token.");
+            throw new BaseException(JwtErrorCode.ACCESS_TOKEN_ERROR);
+        }
+
+        log.debug("학생 정보를 가져오는 데 성공했습니다. : studentId = {}, courseId = {}", studentId, courseId);
+
+        return Map.of(
+                "studentId", studentId,
+                "courseId", courseId
+        );
+    }
+
+    private Claims getClaims(String token) {
         try {
-            claims = Jwts.parserBuilder()
+            return Jwts.parserBuilder()
                     .setSigningKey(secretKey)
                     .build()
                     .parseClaimsJws(token)
@@ -89,22 +144,5 @@ public class JwtTokenUtil {
             log.debug("JWT 토큰으로부터 사용자 정보를 가져오는 과정에서 발생한 에러입니다. : {}", e.getMessage());
             throw new BaseException(JwtErrorCode.ACCESS_TOKEN_ERROR);
         }
-
-        String oauthId = claims.get("oauthId", String.class);
-        String email = claims.get("email", String.class);
-        Boolean isSignedUp = claims.get("isSignedUp", Boolean.class);
-
-        if (oauthId == null || email == null || isSignedUp == null) {
-            log.debug("JWT 토큰으로부터 사용자 정보를 가져오는 과정에서 발생한 에러입니다. : Missing required claims in JWT token.");
-            throw new BaseException(JwtErrorCode.ACCESS_TOKEN_ERROR);
-        }
-
-        log.debug("사용자 정보를 가져오는 데 성공했습니다. : oauthId = {}, email = {}, isSignedUp = {}", oauthId, email, isSignedUp);
-
-        return Map.of(
-                "oauthId", oauthId,
-                "email", email,
-                "isSignedUp", isSignedUp
-        );
     }
 }
