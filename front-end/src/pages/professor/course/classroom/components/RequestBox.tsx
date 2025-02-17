fa8269a2573cfa769c41f3fb0c9dd22bf572c1a9
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-02-17 14:48:28 +0900

    Feat: Request Box 에러 핸들링 추가 #141
    - request가 없을 때 Request Box 렌더링 추가

--- front-end/src/pages/professor/course/classroom/components/RequestBox.tsx
+++ front-end/src/pages/professor/course/classroom/components/RequestBox.tsx
@@ -1,4 +1,11 @@
-import { Requests } from '@/core/model';
+import {
+  Requests,
+  RequestHard,
+  RequestFast,
+  RequestQuestion,
+  RequestSize,
+  RequestSound,
+} from '@/core/model';
 import S from './RequestBox.module.css';
 import BulbEmoji from '@/assets/icons/bulb-emoji.svg?react';
 import EarEmoji from '@/assets/icons/ear-emoji.svg?react';
@@ -20,7 +27,43 @@ const EmojiType = {
 };
 
 const RequestBox = ({ request }: RequestBoxProps) => {
-  return (
+  return request.length === 0 ? (
+    <div className={S.container}>
+      <RequestCard
+        Emoji={WishEmoji}
+        title={RequestHard.title}
+        description={RequestHard.description}
+        count={0}
+      />
+      <RequestCard
+        Emoji={WindEmoji}
+        title={RequestFast.title}
+        description={RequestFast.description}
+        count={0}
+      />
+      <RequestCard
+        Emoji={BulbEmoji}
+        title={RequestQuestion.title}
+        description={RequestQuestion.description}
+        count={0}
+      />
+      <RequestCard
+        Emoji={MagnifierEmoji}
+        title={RequestSize.title}
+        description={RequestSize.description}
+        count={0}
+      />
+      <RequestCard
+        Emoji={EarEmoji}
+        title={RequestSound.title}
+        description={RequestSound.description}
+        count={0}
+      />
+      <div className={S.popup}>
+        <span>오늘 누적 반응 학생 수</span>
+      </div>
+    </div>
+  ) : (
     <div className={S.container}>
       {request.map((request, index) => (
         <RequestCard
