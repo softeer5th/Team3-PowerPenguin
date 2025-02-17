3952393a661cf1f02b8762784fdefd08a63c49fa
uri010 <dbfl0461@gmail.com>
2025-02-17 15:30:46 +0900

    Feat: S3 파일 업로드 #57
    - 파일명 충돌 방지를 위해 UUID 활용하여 고유 파일명 생성
    - 파일 저장시 폴더 지정

---
+++ back-end/reacton/src/main/java/com/softeer/reacton/global/s3/S3Service.java
@@ -0,0 +1,53 @@
+package com.softeer.reacton.global.s3;
+
+import com.softeer.reacton.global.exception.BaseException;
+import com.softeer.reacton.global.exception.code.FileErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+    private final S3Client s3Client;
+
+    @Value("${aws.bucket.name}")
+    private String bucketName;
+
+    public String uploadFile(MultipartFile file, String folder) {
+        String fileName = folder + generateFileName(file);
+        try {
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(fileName)
+                    .contentType(file.getContentType())
+                    .contentLength(file.getSize())
+                    .build();
+
+            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+            log.info("S3 파일 업로드 완료: {}", fileName);
+            return fileName;
+        } catch (IOException e) {
+            log.error(FileErrorCode.FILE_READ_FAILED.getMessage());
+            throw new BaseException(FileErrorCode.FILE_READ_FAILED);
+        } catch (S3Exception e) {
+            log.error("S3 파일 업로드 실패: {}", e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    private String generateFileName(MultipartFile file) {
+        return UUID.randomUUID() + "." + file.getOriginalFilename();
+    }
+}
