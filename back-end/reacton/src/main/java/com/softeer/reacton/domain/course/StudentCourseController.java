1b57b2c307de13bae301ee3fa66ce692a17e4c50
sunohkim <sunoh1222@gmail.com>
2025-02-17 14:16:39 +0900

    Feat: 학생 수업 참가 요청 API 구현 #136
    - 수업이 존재하고, 현재 열려 있는지 확인
    - 열려 있는 수업이 존재하면 임시 ID 발급 후 JWT 토큰을 쿠키 형태로 제공
    - 강의실 페이지로 리다이렉트

--- back-end/reacton/src/main/java/com/softeer/reacton/domain/course/StudentCourseController.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/course/StudentCourseController.java
@@ -1,18 +1,18 @@
 package com.softeer.reacton.domain.course;
 
 import com.softeer.reacton.domain.course.dto.CourseSummaryResponse;
+import com.softeer.reacton.global.config.CookieConfig;
 import com.softeer.reacton.global.dto.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentCourseController {
 
     private final StudentCourseService studentCourseService;
+    private final CookieConfig cookieConfig;
 
     @GetMapping("/{accessCode}/summary")
     @Operation(
@@ -46,5 +47,36 @@ public class StudentCourseController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(SuccessResponse.of("성공적으로 조회했습니다.", response));
+    }
+
+    @PostMapping("/{accessCode}/in")
+    @Operation(
+            summary = "학생 수업 참가 요청",
+            description = "학생이 입력한 수업 코드에 해당하는 수업 참가를 요청합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "303", description = "수업 참여를 위해 강의실로 이동합니다.")
+            }
+    )
+    public ResponseEntity<Void> registerCourse(
+            @PathVariable("accessCode") int accessCode
+    ) {
+        log.debug("입장 코드와 일치하는 수업 참여를 요청합니다. : accessCode = {}", accessCode);
+
+        String newAccessToken = studentCourseService.registerCourse(accessCode);
+        ResponseCookie jwtCookie = ResponseCookie.from("access_token", newAccessToken)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(cookieConfig.getStudentAccessExpiration())
+                .sameSite("Strict")
+                .build();
+
+        log.info("수업 참여에 성공했습니다.");
+
+        return ResponseEntity
+                .status(HttpStatus.SEE_OTHER)
+                .header(HttpHeaders.LOCATION, "/students/classroom")
+                .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .build();
     }
 }
