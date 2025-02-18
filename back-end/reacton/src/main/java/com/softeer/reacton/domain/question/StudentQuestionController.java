75e98d36463802847b17d0fe3c30ceabeb8ad2d4
sunohkim <sunoh1222@gmail.com>
2025-02-18 21:25:40 +0900

    Feat: 학생이 질문을 보내는 API 구현 #154
    - jwt 토큰으로부터 courseId, studentId 추출
    - 입력받은 질문 내용을 데이터베이스에 저장
    - 비동기 방식으로 SSE 서버에 질문 전송 요청
    - 클라이언트에게는 데이터베이스 저장 성공 여부를 응답

--- back-end/reacton/src/main/java/com/softeer/reacton/domain/question/StudentQuestionController.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/question/StudentQuestionController.java
@@ -1,6 +1,8 @@
 package com.softeer.reacton.domain.question;
 
-import com.softeer.reacton.domain.question.dto.QuestionResponse;
+import com.softeer.reacton.domain.course.dto.CourseQuestionResponse;
+import com.softeer.reacton.domain.question.dto.QuestionAllResponse;
+import com.softeer.reacton.domain.question.dto.QuestionSendRequest;
 import com.softeer.reacton.global.dto.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -10,9 +12,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -34,18 +34,47 @@ public class StudentQuestionController {
                     @ApiResponse(responseCode = "500", description = "서버와의 연결에 실패했습니다.")
             }
     )
-    public ResponseEntity<SuccessResponse<QuestionResponse>> getQuestions(HttpServletRequest request) {
+    public ResponseEntity<SuccessResponse<QuestionAllResponse>> getQuestions(HttpServletRequest request) {
         log.debug("학생 사용자가 이전에 질문했던 목록을 요청합니다.");
 
         String studentId = (String) request.getAttribute("studentId");
         Long courseId = (Long) request.getAttribute("courseId");
 
-        QuestionResponse response = studentQuestionService.getQuestionsByStudentId(studentId, courseId);
+        QuestionAllResponse response = studentQuestionService.getQuestionsByStudentId(studentId, courseId);
 
         log.info("질문 목록을 성공적으로 조회했습니다.");
 
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(SuccessResponse.of("성공적으로 조회했습니다.", response));
+    }
+
+    @PostMapping
+    @Operation(
+            summary = "학생 질문 전송",
+            description = "학생이 교수에게 질문을 전송합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "성공적으로 전송했습니다."),
+                    @ApiResponse(responseCode = "404", description = "수업을 찾을 수 없습니다."),
+                    @ApiResponse(responseCode = "409", description = "아직 수업이 시작되지 않았습니다."),
+                    @ApiResponse(responseCode = "500", description = "서버와의 연결에 실패했습니다.")
+            }
+    )
+    public ResponseEntity<SuccessResponse<CourseQuestionResponse>> sendQuestion(
+            @RequestBody QuestionSendRequest questionSendRequest,
+            HttpServletRequest request) {
+        log.debug("학생 사용자가 질문 등록 및 전송을 요청합니다.");
+
+        String studentId = (String) request.getAttribute("studentId");
+        Long courseId = (Long) request.getAttribute("courseId");
+        String content = questionSendRequest.getContent();
+
+        CourseQuestionResponse response = studentQuestionService.sendQuestion(studentId, courseId, content);
+
+        log.info("질문을 성공적으로 등록했습니다.");
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(SuccessResponse.of("성공적으로 등록했습니다.", response));
     }
 }
--- back-end/reacton/src/main/java/com/softeer/reacton/domain/question/StudentQuestionService.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/question/StudentQuestionService.java
@@ -3,9 +3,11 @@ package com.softeer.reacton.domain.question;
 import com.softeer.reacton.domain.course.Course;
 import com.softeer.reacton.domain.course.CourseRepository;
 import com.softeer.reacton.domain.course.dto.CourseQuestionResponse;
-import com.softeer.reacton.domain.question.dto.QuestionResponse;
+import com.softeer.reacton.domain.question.dto.QuestionAllResponse;
 import com.softeer.reacton.global.exception.BaseException;
 import com.softeer.reacton.global.exception.code.CourseErrorCode;
+import com.softeer.reacton.global.sse.SseMessageSender;
+import com.softeer.reacton.global.sse.dto.SseMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,15 +23,47 @@ public class StudentQuestionService {
 
     private final QuestionRepository questionRepository;
     private final CourseRepository courseRepository;
+    private final SseMessageSender sseMessageSender;
 
     @Transactional(readOnly = true)
-    public QuestionResponse getQuestionsByStudentId(String studentId, Long courseId) {
+    public QuestionAllResponse getQuestionsByStudentId(String studentId, Long courseId) {
         log.debug("이전에 질문했던 목록을 조회합니다. : studentId = {}, courseId = {}", studentId, courseId);
 
         Course course = getCourse(courseId);
         List<CourseQuestionResponse> questions = findQuestions(studentId, course);
 
-        return QuestionResponse.of(questions);
+        return QuestionAllResponse.of(questions);
+    }
+
+    @Transactional
+    public CourseQuestionResponse sendQuestion(String studentId, Long courseId, String content) {
+        log.debug("질문 처리를 시작합니다. : content = {}", content);
+
+        Course course = getCourse(courseId);
+        checkIfOpen(course);
+
+        Question question = Question.builder()
+                .studentId(studentId)
+                .content(content)
+                .course(course)
+                .build();
+
+        log.debug("질문을 저장합니다.");
+        System.out.println("question.isComplete: " + question.getIsComplete());
+        Question savedQuestion = questionRepository.save(question);
+
+        CourseQuestionResponse questionDto = new CourseQuestionResponse(
+                savedQuestion.getId(),
+                savedQuestion.getCreatedAt(),
+                savedQuestion.getContent()
+        );
+
+        log.debug("SSE 서버에 질문 전송을 요청합니다.");
+        SseMessage<CourseQuestionResponse> sseMessage = new SseMessage<>("QUESTION", questionDto);
+        sseMessageSender.sendMessageToProfessor(courseId, sseMessage);
+
+        log.debug("질문 처리가 완료되었습니다.");
+        return questionDto;
     }
 
     private Course getCourse(Long courseId) {
@@ -47,5 +81,11 @@ public class StudentQuestionService {
                         question.getContent()
                 ))
                 .collect(Collectors.toList());
+    }
+
+    private void checkIfOpen(Course course) {
+        if (!course.isActive()) {
+            throw new BaseException(CourseErrorCode.COURSE_NOT_ACTIVE);
+        }
     }
 }
