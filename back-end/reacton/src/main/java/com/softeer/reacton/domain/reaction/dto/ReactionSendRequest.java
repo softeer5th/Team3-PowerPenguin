215ee1780cf430e36199404a8f92044324a2a1a8
sunohkim <sunoh1222@gmail.com>
2025-02-19 17:10:52 +0900

    Fix: 요청, 반응 Request dto 상수화 #154

--- back-end/reacton/src/main/java/com/softeer/reacton/domain/reaction/dto/ReactionSendRequest.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/reaction/dto/ReactionSendRequest.java
@@ -1,14 +1,16 @@
 package com.softeer.reacton.domain.reaction.dto;
 
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ReactionSendRequest {
 
     @Pattern(
             regexp = "^(OKAY|CLAP|THUMBS_UP|HEART_EYES|CRYING|SURPRISED)$",
             message = "반응에 들어갈 값이 잘못되었습니다."
     )
-    private String content;
+    private final String content;
 }
