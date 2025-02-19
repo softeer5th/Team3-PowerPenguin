91a3056da9783a6a16155ddc365e658492045ef2
sunohkim <sunoh1222@gmail.com>
2025-02-20 01:35:07 +0900

    Feat: 학생 질문 체크 API 구현 #182
    - 학생 사용자가 질문 체크 시 교수에게 SSE 메시지로 전송

--- back-end/reacton/src/main/java/com/softeer/reacton/domain/question/StudentQuestionService.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/question/StudentQuestionService.java
@@ -4,9 +4,11 @@ import com.softeer.reacton.domain.course.Course;
 import com.softeer.reacton.domain.course.CourseRepository;
 import com.softeer.reacton.domain.course.dto.CourseQuestionResponse;
 import com.softeer.reacton.domain.question.dto.QuestionAllResponse;
+import com.softeer.reacton.domain.question.dto.QuestionCheckSseRequest;
 import com.softeer.reacton.domain.question.dto.QuestionSendRequest;
 import com.softeer.reacton.global.exception.BaseException;
 import com.softeer.reacton.global.exception.code.CourseErrorCode;
+import com.softeer.reacton.global.exception.code.QuestionErrorCode;
 import com.softeer.reacton.global.sse.SseMessageSender;
 import com.softeer.reacton.global.sse.dto.SseMessage;
 import lombok.RequiredArgsConstructor;
@@ -61,10 +63,32 @@ public class StudentQuestionService {
 
         log.debug("SSE 서버에 질문 전송을 요청합니다.");
         SseMessage<CourseQuestionResponse> sseMessage = new SseMessage<>("QUESTION", courseQuestionResponse);
-        sseMessageSender.sendMessageToProfessor(courseId, sseMessage);
+        sseMessageSender.sendMessage(courseId.toString(), sseMessage);
 
         log.debug("질문 처리가 완료되었습니다.");
         return courseQuestionResponse;
+    }
+
+    @Transactional
+    public void sendQuestionCheck(String studentId, Long courseId, Long questionId) {
+        log.debug("질문 체크 처리를 시작합니다. : questionId = {}", questionId);
+
+        Course course = getCourse(courseId);
+        checkIfOpen(course);
+
+        int updatedRows = questionRepository.updateQuestion(studentId, course, questionId);
+        if (updatedRows == 0) {
+            log.debug("질문 체크를 처리하는 과정에서 발생한 에러입니다. : Question does not exist.");
+            throw new BaseException(QuestionErrorCode.QUESTION_NOT_FOUND);
+        }
+
+        QuestionCheckSseRequest questionCheckSseRequest = new QuestionCheckSseRequest(questionId);
+
+        log.debug("SSE 서버에 질문 체크 전송을 요청합니다.");
+        SseMessage<QuestionCheckSseRequest> sseMessage = new SseMessage<>("QUESTION_CHECK", questionCheckSseRequest);
+        sseMessageSender.sendMessage(courseId.toString(), sseMessage);
+
+        log.debug("질문 체크 처리가 완료되었습니다.");
     }
 
     private Course getCourse(Long courseId) {
