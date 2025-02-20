c60a63ad5e850a6aa43dfd3f986453b18ba459eb
sunohkim <sunoh1222@gmail.com>
2025-02-20 14:11:52 +0900

    Fix: 불필요한 예외 핸들러 제거 #182

--- back-end/reacton/src/main/java/com/softeer/reacton/global/sse/SseMessageSender.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/global/sse/SseMessageSender.java
@@ -1,7 +1,5 @@
 package com.softeer.reacton.global.sse;
 
-import com.softeer.reacton.global.exception.BaseException;
-import com.softeer.reacton.global.exception.code.SseErrorCode;
 import com.softeer.reacton.global.sse.dto.SseMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,28 +23,22 @@ public class SseMessageSender {
 
     public void sendMessage(String id, SseMessage<?> message) {
         String url = sseMessageBaseUrl + "course/" + id;
-        try {
-            webClient.post()
-                    .uri(url, id)
-                    .bodyValue(message)
-                    .retrieve()
-                    .bodyToMono(Void.class)
-                    .doOnSuccess(aVoid ->
-                            log.debug("SSE 메시지 전송에 성공했습니다. : courseId = {}", id))
-                    .retryWhen(Retry.backoff(2, Duration.ofSeconds(1))
-                            .filter(throwable -> {
-                                log.debug("재시도 중입니다.");
-                                return throwable instanceof WebClientRequestException;
-                            }))
-                    .onErrorResume(error -> {
-                        log.warn("SSE 메시지 전송에 실패했습니다. : courseId = {}, messageType = {}, error = {}",
-                                id, message.getMessageType(), error.getMessage());
-                        return Mono.empty();
-                    })
-                    .subscribe(); // 비동기적으로 처리
-        } catch (Exception e) {
-            log.error("SSE 메시지 전송 중 예외가 발생했습니다. : {}", e.getMessage());
-            throw new BaseException(SseErrorCode.MESSAGE_SEND_FAILURE);
-        }
+        webClient.post()
+                .uri(url, id)
+                .bodyValue(message)
+                .retrieve()
+                .bodyToMono(Void.class)
+                .doOnSuccess(aVoid -> log.debug("SSE 메시지 전송에 성공했습니다. : courseId = {}", id))
+                .retryWhen(Retry.backoff(2, Duration.ofSeconds(1))
+                        .filter(throwable -> {
+                            log.debug("재시도 중입니다.");
+                            return throwable instanceof WebClientRequestException;
+                        }))
+                .onErrorResume(error -> {
+                    log.warn("SSE 메시지 전송에 실패했습니다. : courseId = {}, messageType = {}, error = {}",
+                            id, message.getMessageType(), error.getMessage());
+                    return Mono.empty();
+                })
+                .subscribe();
     }
 }
