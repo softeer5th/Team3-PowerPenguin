8081a7c1683e0402acb587d37f6ea24ccd382fa4
uri010 <dbfl0461@gmail.com>
2025-02-24 14:47:28 +0900

    Feat: CORS 설정 추가 #238

---
+++ back-end/reacton-classroom/src/main/java/com/softeer/reacton_classroom/global/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.softeer.reacton_classroom.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("https://softeer-reacton.shop")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
