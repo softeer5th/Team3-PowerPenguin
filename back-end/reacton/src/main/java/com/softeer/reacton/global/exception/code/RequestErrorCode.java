93036ccfcfd46c8defa66592eadf7fce85491848
sunohkim <sunoh1222@gmail.com>
2025-02-24 20:23:40 +0900

    Feat: 요청 횟수 오버플로우에 대한 에러코드 추가 #229

--- back-end/reacton/src/main/java/com/softeer/reacton/global/exception/code/RequestErrorCode.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/global/exception/code/RequestErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum RequestErrorCode implements ErrorCode {
-    REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "요청 데이터를 찾을 수 없습니다.");
+    REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "요청 데이터를 찾을 수 없습니다."),
+    REQUEST_OVERFLOW(HttpStatus.BAD_REQUEST, "요청 횟수를 초과했습니다.");
 
     private final HttpStatus status;
     private final String message;
