e9d7fca59e567334c5f25098df7384f47f9ba510
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-02-26 12:45:10 +0900

    Fix: 수업 유효성 항목 추가 #263
    - 강의명, 대학명, 학수번호 유효성 검사 추가
    - 강의 명이 숫자일 경우 줄바꿈 적용 안 되는 버그 수정

--- front-end/src/pages/professor/home/modal/CourseModal.tsx
+++ front-end/src/pages/professor/home/modal/CourseModal.tsx
@@ -240,6 +240,11 @@ const CourseModal = ({ course, onSubmit, onClose }: CourseModalProps) => {
                 ...prev,
                 name: '강의 이름을 입력해 주세요',
               }));
+            } else if (courseForm.name.length > 35) {
+              setFormError((prev) => ({
+                ...prev,
+                name: '강의 이름은 30자 이하로 입력해 주세요',
+              }));
             } else {
               setFormError((prev) => ({ ...prev, name: '' }));
             }
@@ -333,6 +338,11 @@ const CourseModal = ({ course, onSubmit, onClose }: CourseModalProps) => {
                 ...prev,
                 code: '학수번호를 입력해 주세요',
               }));
+            } else if (courseForm.code.length > 10) {
+              setFormError((prev) => ({
+                ...prev,
+                code: '학수번호는 10자 이하로 입력해 주세요',
+              }));
             } else {
               setFormError((prev) => ({ ...prev, code: '' }));
             }
@@ -383,6 +393,11 @@ const CourseModal = ({ course, onSubmit, onClose }: CourseModalProps) => {
               setFormError((prev) => ({
                 ...prev,
                 university: '대학이름을 입력해 주세요',
+              }));
+            } else if (courseForm.university.length > 20) {
+              setFormError((prev) => ({
+                ...prev,
+                university: '대학이름은 20자 이하로 입력해 주세요',
               }));
             } else {
               setFormError((prev) => ({ ...prev, university: '' }));
